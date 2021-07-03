fd8b4e0245a464552de246f9ae2cc6c507b01bbb
Lucas Ethan <asmrit1010@gmail.com>
2021-07-03 10:01:13 +0200

    Used for loop instead of regex

--- 7kyu/disemvowel.js
+++ 7kyu/disemvowel.js
@@ -1,7 +1,21 @@
 #!/usr/bin/env node
 
-function disemvowel(str) {
-  return str.replace(/[aeiou]/gi, "");
-}
+// function disemvowel(str) {
+  // return str.replace(/[aeiou]/gi, "");
+// }
+
+// console.log(disemvowel("This website is for losers LOL!"));
+
+
+  function disemvowel(str) {
+    let newString = "";
+    for (char of str) {
+      let vowels = "aeiou";
+      if (!vowels.includes(char.toLowerCase())) {
+        newString += char;
+      }
+    }
+    return newString;
+  }
 
 console.log(disemvowel("This website is for losers LOL!"));
