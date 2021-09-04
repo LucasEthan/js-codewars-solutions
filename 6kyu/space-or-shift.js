2bd7efd2eeb8b575099f3abc2ba32134fc4a59d9
Lucas Ethan <asmrit1010@gmail.com>
2021-09-04 09:50:34 +0200

    Attempt 1 of space or shift solution

---
+++ 6kyu/space-or-shift.js
@@ -0,0 +1,15 @@
+function getOrder(input) {
+  const menu = ["burger", "fries", "chicken", "pizza", "sandwich", "onionrings", "milkshake", "coke"]
+  let order = ""
+  let orders = []
+  for (char of input) {
+    order += char
+    if (menu.includes(order)) {
+      orders.push(order)
+      order = ""
+    }
+  }
+  return orders.map((order) => order[0].toUpperCase() + order.slice(1)).join(" ")
+}
+
+console.log(getOrder("milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza"))
