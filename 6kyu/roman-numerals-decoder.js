6a84ce2cfb3074986b3997b74d83b824f7bba179
Lucas Ethan <asmrit1010@gmail.com>
2021-10-23 10:11:03 +0200

    Refactored roman numerals decoder solution

--- 6kyu/roman-numerals-decoder.js
+++ 6kyu/roman-numerals-decoder.js
@@ -1,104 +1,49 @@
 function solution(roman) {
   let num = 0;
 
-  if (roman.startsWith("MMM")) {
-    num += 3000;
-    roman = roman.slice(3);
-  } else if (roman.startsWith("MM")) {
-    num += 2000;
-    roman = roman.slice(2);
-  } else if (roman.startsWith("M")) {
-    num += 1000;
-    roman = roman.slice(1);
-  }
+  const romanConversion = {
+    MMM: 3000,
+    MM: 2000,
+    M: 1000,
 
-  if (roman.startsWith("CM")) {
-    num += 900;
-    roman = roman.slice(2);
-  } else if (roman.startsWith("DCCC")) {
-    num += 800;
-    roman = roman.slice(4);
-  } else if (roman.startsWith("DCC")) {
-    num += 700;
-    roman = roman.slice(3);
-  } else if (roman.startsWith("DC")) {
-    num += 600;
-    roman = roman.slice(2);
-  } else if (roman.startsWith("D")) {
-    num += 500;
-    roman = roman.slice(1);
-  } else if (roman.startsWith("CD")) {
-    num += 400;
-    roman = roman.slice(2);
-  } else if (roman.startsWith("CCC")) {
-    num += 300;
-    roman = roman.slice(3);
-  } else if (roman.startsWith("CC")) {
-    num += 200;
-    roman = roman.slice(2);
-  } else if (roman.startsWith("C")) {
-    num += 100;
-    roman = roman.slice(1);
-  }
+    CM: 900,
+    DCCC: 800,
+    DCC: 700,
+    DC: 600,
+    D: 500,
+    CD: 400,
+    CCC: 300,
+    CC: 200,
+    C: 100,
 
-  if (roman.startsWith("XC")) {
-    num += 90;
-    roman = roman.slice(2);
-  } else if (roman.startsWith("LXXX")) {
-    num += 80;
-    roman = roman.slice(4);
-  } else if (roman.startsWith("LXX")) {
-    num += 70;
-    roman = roman.slice(3);
-  } else if (roman.startsWith("LX")) {
-    num += 60;
-    roman = roman.slice(2);
-  } else if (roman.startsWith("L")) {
-    num += 50;
-    roman = roman.slice(1);
-  } else if (roman.startsWith("XL")) {
-    num += 40;
-    roman = roman.slice(2);
-  } else if (roman.startsWith("XXX")) {
-    num += 30;
-    roman = roman.slice(3);
-  } else if (roman.startsWith("XX")) {
-    num += 20;
-    roman = roman.slice(2);
-  } else if (roman.startsWith("X")) {
-    num += 10;
-    roman = roman.slice(1);
-  }
+    XC: 90,
+    LXXX: 80,
+    LXX: 70,
+    LX: 60,
+    L: 50,
+    XL: 40,
+    XXX: 30,
+    XX: 20,
+    X: 10,
 
-  if (roman.startsWith("IX")) {
-    num += 9;
-    roman = roman.slice(2);
-  } else if (roman.startsWith("VIII")) {
-    num += 8;
-    roman = roman.slice(4);
-  } else if (roman.startsWith("VII")) {
-    num += 7;
-    roman = roman.slice(3);
-  } else if (roman.startsWith("VI")) {
-    num += 6;
-    roman = roman.slice(2);
-  } else if (roman.startsWith("V")) {
-    num += 5;
-    roman = roman.slice(1);
-  } else if (roman.startsWith("IV")) {
-    num += 4;
-    roman = roman.slice(2);
-  } else if (roman.startsWith("III")) {
-    num += 3;
-    roman = roman.slice(3);
-  } else if (roman.startsWith("II")) {
-    num += 2;
-    roman = roman.slice(2);
-  } else if (roman.startsWith("I")) {
-    num += 1;
-    roman = roman.slice(1);
+    IX: 9,
+    VIII: 8,
+    VII: 7,
+    VI: 6,
+    V: 5,
+    IV: 4,
+    III: 3,
+    II: 2,
+    I: 1,
+  };
+
+  for (romanNum in romanConversion) {
+    if (roman.startsWith(romanNum)) {
+      num += romanConversion[romanNum];
+      roman = roman.slice(romanNum.length);
+    }
   }
   return num;
 }
 
-console.log(solution("MMM"));
+console.log(solution("MMCC"));
